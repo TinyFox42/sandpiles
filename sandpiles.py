df1ed4c930bc4834bfc5aff86b8fbc34f0e60e95
Eli Thorpe <elithorpe42@gmail.com>
2019-11-19 10:25:59 -0500

    Defined the settling
    This is going to be quick. I just did 1 of 4 main tasks, the other 3 being:
    *addition of piles
    *iteration through all possible changes
    *getting this into a .csv

---
+++ sandpiles.py
@@ -0,0 +1,48 @@
+#Sandpiles are a cool idea
+#Made by Eli Thorpe
+rows=3
+cols=3
+debug=True #controls the main testing prints
+def check(pile):
+    #checks that the pile is fully settled, i.e. all cells are less than 3 (in a square setup, which is all this will be able to carry for awhile)
+    #Please make sure that the dimensions of this are correct, I'm not planning on covering that up
+    for i in range(rows):
+        for j in range(cols):
+            if pile[i][j]>3:
+                return False
+    return True
+
+def simple(val):
+    #makes a pile where all of the cells have the same value
+    pile=[]
+    for i in range(rows):
+        r=[]
+        for j in range(cols):
+            r.append(val)
+        pile.append(r)
+    return pile
+    
+def settle(pile, show=debug):
+    #goes through one iteration of settling the sandpile, then recurses. 
+    #Show is a debugging option that you may want, prints the pile after every iteration
+    sand=simple(0) #sand is the new sandpile, pile is the old
+    for i in range(rows):
+        for j in range(cols):
+            sand[i][j]+=pile[i][j]%4
+            n=pile[i][j]/4
+            if n>0:
+                if i>0:
+                    sand[i-1][j]+=n
+                if j>0:
+                    sand[i][j-1]+=n
+                if i<rows-1:
+                    sand[i+1][j]+=n
+                if j<cols-1:
+                    sand[i][j+1]+=n
+    if show:
+        print sand
+        print "\n"
+    if check(sand):
+        return sand
+    else:
+        return settle(sand,show)
